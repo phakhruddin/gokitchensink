2c8ab6cc9fc024df21dbd49a3d98c738af88643f
phakhruddin <phakhruddin@gmail.com>
2024-07-17 21:48:32 -0700

    feat(TRAIN): add month

--- test1.go
+++ test1.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	email := "evangeline@educative.io"
 	x := strings.Split(email, "@")
+	z := strings.TrimLeft()
 	y := (strings.Replace("XXX", x[0], "", 5)) + "@" + x[1]
 	fmt.Println(y)
 }
